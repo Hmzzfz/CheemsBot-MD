62f1bf9ac3cf0da606831256b548db0d6a009cac
Hmzzfz <108460694+Hmzzfz@users.noreply.github.com>
2022-07-11 10:32:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['6285893481599','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['6285893481599'] //another owner number
+global.premium = ['6285893481599'] //premium number
+global.pengguna = 'HamzX' //username
+global.botnma = 'HamzX Bot-MD' //bot name
+global.ownernma = 'HamzKhull' //owner name
+global.packname = 'HamzX Bot' //sticker package name
+global.author = 'By HamzX Bot' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
